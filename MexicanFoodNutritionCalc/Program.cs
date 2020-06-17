22fcd9be8fe0774ab4f1240afdf6341d11f8b6ec
Lachlan Jasson <jassonlachlan@gmail.com>
2020-06-18 06:24:37 +0930

    Add project files.

---
+++ MexicanFoodNutritionCalc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MexicanFoodNutritionCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
